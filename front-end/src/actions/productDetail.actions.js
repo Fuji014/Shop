7d9b90f822a58da09fb0ff91edc04bfb8a3285c6
Fuji014 <jeromegabat14@gmail.com>
2020-11-30 02:37:54 -0800

    adding cart functionality

--- front-end/src/actions/productDetail.actions.js
+++
@@ -1,2 +0,0 @@
-import initialAxios from "../helpers/axios";
-import { productDetailsConstants } from "./constants";
--- front-end/src/containers/Cart/index.js
+++ front-end/src/containers/Cart/index.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
+
+// actions
+import { addToCart } from "../../actions/";
+
+// router
+import { Link } from "react-router-dom";
+
+// bootstrap
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
 
 // components
 import Layout from "../../components/Layout";
 
-function Cart() {
+function Cart({ match, location, history }) {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
+  const productId = match.params.id;
+  // ?qty=1
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return (
     <Layout>
       <h1>Cart</h1>
