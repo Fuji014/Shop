36408c5fb7907bf4804a27fe9da8abc92f961f2a
Fuji014 <jeromegabat14@gmail.com>
2020-12-03 18:42:42 -0800

    adding action and reducer in order details

--- front-end/src/actions/order.actions.js
+++ front-end/src/actions/order.actions.js
@@ -25,3 +25,28 @@ export const createOrder = (order) => async (dispatch) => {
     });
   }
 };
+
+export const getOrderDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: orderConstants.ORDER_DETAILS_REQUEST,
+    });
+
+    const res = await initialAxios.get(`/orders/${id}`);
+
+    dispatch({
+      type: orderConstants.ORDER_DETAILS_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: orderConstants.ORDER_DETAILS_FAILURE,
+      payload: {
+        error:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      },
+    });
+  }
+};
--- front-end/src/reducers/order.reducers.js
+++ front-end/src/reducers/order.reducers.js
@@ -9,6 +9,7 @@ const initialState = {
 
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
+    // order create
     case orderConstants.ORDER_CREATE_REQUEST:
       return {
         ...state,
@@ -27,6 +28,26 @@ const orderReducer = (state = initialState, action) => {
         ...initialState,
         loading: false,
         success: false,
+        error: action.payload.error,
+      };
+
+    // order details
+    case orderConstants.ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case orderConstants.ORDER_DETAILS_SUCCESS:
+      return {
+        ...state,
+        orderDetails: action.payload,
+        loading: false,
+      };
+
+    case orderConstants.ORDER_DETAILS_FAILURE:
+      return {
+        ...initialState,
+        loading: false,
         error: action.payload.error,
       };
 
