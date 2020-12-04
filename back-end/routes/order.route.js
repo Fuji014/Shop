396d6c5bb30207014a8062db3d13126d2b6ac6af
Fuji014 <jeromegabat14@gmail.com>
2020-12-03 20:00:37 -0800

    adding order pay endpoint at the backend

--- back-end/routes/order.route.js
+++ back-end/routes/order.route.js
@@ -7,12 +7,14 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
-} = require("../controllers/order.conteroller");
+  updateOrderToPaid,
+} = require("../controllers/order.controller");
 
 // middleware
 const { protect } = require("../middleware/auth.middleware");
 
 router.route("/orders").post(protect, addOrderItems);
 router.route("/orders/:id").get(protect, getOrderById);
+router.route("/orders/:id/pay").put(protect, updateOrderToPaid);
 
 module.exports = router;
