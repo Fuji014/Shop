1bfbfb1a17baf25f50f52083de54a26e7c95424d
Fuji014 <jeromegabat14@gmail.com>
2020-12-11 03:37:50 -0800

    try to fix error (no such file or dir) attempt 1

--- back-end/server.js
+++ back-end/server.js
@@ -29,10 +29,10 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/front-end/build")));
+  app.use(express.static(path.join(__dirname, "../front-end/build")));
 
   app.get("/", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "front-end", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../front-end", "build", "index.html"))
   );
 }
 app.use("/uploads", express.static(path.resolve(__dirname, "../uploads")));
