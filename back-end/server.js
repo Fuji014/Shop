5a4d170156b50bb9a27510afcc5f8f7bd6208ae4
Fuji014 <jeromegabat14@gmail.com>
2020-12-11 01:05:03 -0800

    preapare for deployment

--- back-end/server.js
+++ back-end/server.js
@@ -29,9 +29,9 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../front-end/build")));
+  app.use(express.static(path.join(__dirname, "../front-end/build")));
 
-  app.get("*", (req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../front-end", "build", "index.html"))
   );
 }
